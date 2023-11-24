135e5b53eaca7ff40d77a9db79e52f43f903d9ba
project <project@project.myguest.virtualbox.org>
2023-11-24 20:49:54 +0100

    add log types and communication between database and core

--- Core/Logger.cpp
+++ Core/Logger.cpp
@@ -1,14 +1,59 @@
+#include <string>
+#include <iostream>
+
 class Logger {
 public:
+    enum class LogLevel {
+        INFO,
+        WARN,
+        ERROR,
+        DEBUG
+    };
+
     static Logger& getInstance() {
         static Logger instance;
         return instance;
     }
 
-    void log(const std::string& message) {
-        // Implémentation de la logique de log
+    void setLevel(LogLevel level) {
+        this->level = level;
+    }
+
+    void log(LogLevel messageLevel, const std::string& message) {
+        if (messageLevel >= level) {
+            // Implémentation de la logique de log, exemple:
+            std::cout << "[" << toString(messageLevel) << "] " << message << std::endl;
+        }
+    }
+
+    // Méthodes spécifiques pour chaque niveau
+    void info(const std::string& message) {
+        log(LogLevel::INFO, message);
+    }
+
+    void warn(const std::string& message) {
+        log(LogLevel::WARN, message);
+    }
+
+    void error(const std::string& message) {
+        log(LogLevel::ERROR, message);
+    }
+
+    void debug(const std::string& message) {
+        log(LogLevel::DEBUG, message);
     }
 
 private:
-    Logger() {}  // Constructeur privé
+    Logger() {}
+    LogLevel level = LogLevel::INFO;
+
+    std::string toString(LogLevel level) {
+        switch (level) {
+            case LogLevel::INFO:  return "INFO";
+            case LogLevel::WARN:  return "WARN";
+            case LogLevel::ERROR: return "ERROR";
+            case LogLevel::DEBUG: return "DEBUG";
+            default: return "UNKNOWN";
+        }
+    }
 };
--- Core/MemoryManager.cpp
+++ Core/MemoryManager.cpp
@@ -1,38 +1,44 @@
-#include <cstddef>
 #include <iostream>
 #include <unordered_map>
+#include <memory>
+#include <cstdlib>
 
 class MemoryManager {
-public:
-    static MemoryManager& getInstance() {
-        static MemoryManager instance;
-        return instance;
-    }
-
-    void* allocate(std::size_t size) {
-        void* ptr = std::malloc(size);
-        allocations[ptr] = size;
-        std::cout << "Allocated " << size << " bytes at " << ptr << std::endl;
-        return ptr;
-    }
-
-    void deallocate(void* ptr) {
-        if (allocations.find(ptr) != allocations.end()) {
-            std::cout << "Deallocated " << allocations[ptr] << " bytes from " << ptr << std::endl;
-            std::free(ptr);
-            allocations.erase(ptr);
-        } else {
-            std::cout << "Attempt to deallocate unrecognized memory: " << ptr << std::endl;
-        }
-    }
-
-    // Add more functionality as needed
-
 private:
-    MemoryManager() {}
-    ~MemoryManager() {
-        // Cleanup code if required
+    DataBase* db;
+    std::unordered_map<int, std::unique_ptr<std::string>> cache;
+
+public:
+    explicit MemoryManager(DataBase* db) : db(db) {}
+
+    // Creation pointeur unique
+    template<typename T>
+    std::unique_ptr<T, void(*)(void*)> makeUniquePtr(T* ptr) {
+        return std::unique_ptr<T, void(*)(void*)>(ptr, [](void* p) { std::free(p); });
     }
 
-    std::unordered_map<void*, std::size_t> allocations;
+    // Méthode pour allouer de la mémoire
+    template<typename T>
+    std::unique_ptr<T, void(*)(void*)> allocate() {
+        T* ptr = static_cast<T*>(std::malloc(sizeof(T)));
+        if (!ptr) {
+            throw std::bad_alloc();
+        }
+        return makeUniquePtr(ptr);
+    }
+
+    const std::string& getData(int key) {
+        if (cache.find(key) == cache.end()) {
+            cache[key] = std::make_unique<std::string>(db->getData(key));
+        }
+        return *cache[key];
+    }
+
+    // Autres méthodes...
+
+    MemoryManager(const MemoryManager&) = delete;
+    MemoryManager& operator=(const MemoryManager&) = delete;
+    MemoryManager(MemoryManager&&) noexcept = default;
+    MemoryManager& operator=(MemoryManager&&) noexcept = default;
 };
+
