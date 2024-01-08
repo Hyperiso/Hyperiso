cc9a2ad214be16158e890ac00ed7e7075cb1bfcd
Ryudoro <93795941+Ryudoro@users.noreply.github.com>
2024-01-08 16:41:51 +0100

    Update QCDParameters.h
    change stuff in running alphas

--- Physical_Model/QCDParameters.h
+++ Physical_Model/QCDParameters.h
@@ -8,9 +8,14 @@ public:
     double Lambda3, Lambda4, Lambda5, Lambda6;
     double alphasMZ_Lambda3, alphasMZ_Lambda4, alphasMZ_Lambda5, alphasMZ_Lambda6;
     double mass_c, mass_b, mass_t;
+    const double pi = 3.14159265358979323846;
 
-    double calculateLambda(double MZ, double alphas_MZ, int nf);
+    QCDParameters();
+
+    double alphasRunning(double Q, double Lambda, int nf);
+    double DichotomieLambda(double alpha_running, double Q, int nf);
     double runningAlphasCalculation(double Q, double Lambda, int nf);
     double alphas_running(double Q, double mtop, double mbot);
+    double computeAlphas(double Q, double Lambda, int nf);
 
 };
