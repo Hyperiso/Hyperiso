7d283cecc496b962232892e806109a2b029ab63c
Ryudoro <93795941+Ryudoro@users.noreply.github.com>
2024-01-09 16:23:49 +0100

    Update main.cpp
    just testing

--- main.cpp
+++ main.cpp
@@ -5,15 +5,12 @@ int main() {
 
     QCDParameters a;
 
-    double mb =4.4;
-    double mtop = 170;
-
-    double lambda_1 = a.calculateLambda(mtop, mb,1);
-    double lambda_2 = a.calculateLambda(mtop, mb,2);
-    double lambda_3 = a.calculateLambda(mtop, mb,3);
-    double lambda_4 = a.calculateLambda(mtop, mb,4);
-    double lambda_5 = a.calculateLambda(mtop, mb,5);
-    double lambda_6 = a.calculateLambda(mtop, mb,6);
+    double lambda_1 = a.runningAlphasCalculation(1);
+    double lambda_2 = a.runningAlphasCalculation(50);
+    double lambda_3 = a.runningAlphasCalculation(100);
+    double lambda_4 = a.runningAlphasCalculation(175);
+    double lambda_5 = a.runningAlphasCalculation(201);
+    double lambda_6 = a.runningAlphasCalculation(300);
 
     std::cout << lambda_1 << std::endl;
     std::cout << lambda_2 << std::endl;
