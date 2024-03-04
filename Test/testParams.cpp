3e8cc9bac175024847c68afdf82d47502ec0acd2
ArthKuntz <nfardeau@DESKTOP-U6KU7DG>
2024-03-04 17:18:23 +0100

    Add test for SM params initialization

---
+++ Test/testParams.cpp
@@ -0,0 +1,14 @@
+#include "MemoryManager.h"
+#include "Parameters.h"
+
+#include <iostream>
+#include <cassert>
+
+int main() {
+    auto mm = MemoryManager::GetInstance("../example.flha", {0});  // Initialize program manager with LHA file containing SMINPUTS block
+    mm->init();  // Initialize parameters from given LHA file
+
+    auto sm_params = Parameters::GetInstance(0); 
+    double alpha_s_MZ = std::pow((*sm_params)("Coupling", 3), 2) / (4 * M_PI);
+    assert(alpha_s_MZ == 0.12);  // coupling[3] is g_s
+}
