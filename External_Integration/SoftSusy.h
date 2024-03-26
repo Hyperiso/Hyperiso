66c6a52b3e5c16170396fa90fa74ced5c170aece
Supersymmetry <supersymmetry@calculator.myguest.virtualbox.org>
2024-03-26 14:23:30 +0100

    correct little error again

--- External_Integration/SoftSusy.h
+++ External_Integration/SoftSusy.h
@@ -15,6 +15,39 @@ public:
 };
 
 /**
+ * @class CalculateSpectrumCommand
+ * @brief Command to calculate the spectrum using a given calculator.
+ *
+ * Encapsulates a request to calculate the particle spectrum, allowing the
+ * command to be parameterized with different calculators and input/output paths.
+ */
+class CalculateSpectrumCommand : public ICommand {
+private:
+    SoftsusyCalculator& calculator;
+    std::string inputFilePath;
+    std::string outputFilePath;
+
+public:
+    /**
+     * Construct a new Calculate Spectrum Command object.
+     * 
+     * @param calculator Reference to the SoftsusyCalculator.
+     * @param inputFilePath Path to the input file.
+     * @param outputFilePath Path to the output file.
+     */
+    CalculateSpectrumCommand(SoftsusyCalculator& calculator, std::string inputFilePath, std::string outputFilePath)
+        : calculator(calculator), inputFilePath(std::move(inputFilePath)), outputFilePath(std::move(outputFilePath)) {}
+
+    /**
+     * Execute the command to calculate the spectrum.
+     */
+    void execute() override {
+        calculator.calculateSpectrum(inputFilePath, outputFilePath);
+    }
+};
+
+
+/**
  * @class SoftsusyCalculatorFactory
  * @brief Factory class to create instances of Softsusy calculators.
  *
@@ -78,36 +111,5 @@ public:
     virtual ~ICommand() = default;
 };
 
-/**
- * @class CalculateSpectrumCommand
- * @brief Command to calculate the spectrum using a given calculator.
- *
- * Encapsulates a request to calculate the particle spectrum, allowing the
- * command to be parameterized with different calculators and input/output paths.
- */
-class CalculateSpectrumCommand : public ICommand {
-private:
-    SoftsusyCalculator& calculator;
-    std::string inputFilePath;
-    std::string outputFilePath;
-
-public:
-    /**
-     * Construct a new Calculate Spectrum Command object.
-     * 
-     * @param calculator Reference to the SoftsusyCalculator.
-     * @param inputFilePath Path to the input file.
-     * @param outputFilePath Path to the output file.
-     */
-    CalculateSpectrumCommand(SoftsusyCalculator& calculator, std::string inputFilePath, std::string outputFilePath)
-        : calculator(calculator), inputFilePath(std::move(inputFilePath)), outputFilePath(std::move(outputFilePath)) {}
-
-    /**
-     * Execute the command to calculate the spectrum.
-     */
-    void execute() override {
-        calculator.calculateSpectrum(inputFilePath, outputFilePath);
-    }
-};
 
 
