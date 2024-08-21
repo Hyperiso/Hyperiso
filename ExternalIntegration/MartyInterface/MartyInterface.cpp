272c675b2a25ac814fe5afcfcb3a4eabf097714f
Theo Reymermier <treymermier@gmx.fr>
2024-08-21 18:11:42 +0200

    add marty stuff

--- ExternalIntegration/MartyInterface/MartyInterface.cpp
+++ ExternalIntegration/MartyInterface/MartyInterface.cpp
@@ -1,18 +1,25 @@
 #include "MartyInterface.h"
 #include <memory>
+#include "config.hpp"
 
 void MartyInterface::run() {
-    std::unique_ptr<ModelModifier> smModifier = std::make_unique<SMModelModifier>();
-    std::unique_ptr<ModelModifier> thdmModifier = std::make_unique<THDMModelModifier>();
+    
 
-    TemplateManager templateManager("templates");
+    GppCompilerStrategy compiler;
+    compiler.compile("generated_main.cpp", "main");
+}
+
+void MartyInterface::generate() {
+
+    std::unique_ptr<ModelModifier> smModifier = std::make_unique<SMModelModifier>();
+    // std::unique_ptr<ModelModifier> thdmModifier = std::make_unique<THDMModelModifier>();
+
+    std::string root_path = project_root.data();
+    TemplateManager templateManager(root_path+"/DataBase/MartyTemplate");
 
     templateManager.setModelModifier(std::move(smModifier));
     // templateManager.setModelModifier(std::move(thdmModifier));
 
     CodeGenerator codeGenerator(templateManager);
     codeGenerator.generate("C7", "generated_main.cpp");
-
-    GppCompilerStrategy compiler;
-    compiler.compile("generated_main.cpp", "main");
 }
---
+++ ExternalIntegration/MartyInterface/main_marty.cpp
@@ -0,0 +1,9 @@
+#include "MartyInterface.h"
+
+int main() {
+
+    MartyInterface MartyInterface;
+    MartyInterface.generate();
+    MartyInterface.run();
+    return 0;
+}
