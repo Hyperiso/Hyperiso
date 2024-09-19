9fa4ef40c9b18698a7abe946dfab90fa5bda1a1f
Ryudoro <treymermier@gmx.fr>
2024-09-19 09:47:11 +0200

    comment changes

--- ExternalIntegration/MartyInterface/SMParamSetter.cpp
+++ ExternalIntegration/MartyInterface/SMParamSetter.cpp
@@ -3,7 +3,6 @@
 void SMParamSetter::setParam(const std::string& name, const Interpreter::InterpretedParam& interpretedParam) {
     double value = calculateValue(name, interpretedParam);
 
-    // Exemple de logique spécifique : pour certains blocs, on remplace directement par (*sm)(block, value)
     if (interpretedParam.block == "KIN" || interpretedParam.block == "WEIN" || interpretedParam.block == "Finite") {
         std::cout << interpretedParam.block << std::endl;
         params[name] = value;
@@ -14,7 +13,6 @@ void SMParamSetter::setParam(const std::string& name, const Interpreter::Interpr
 }
 
 double SMParamSetter::calculateValue(const std::string& name, const Interpreter::InterpretedParam& interpretedParam) {
-    // Exemple de logique pour les blocs KIN et WEIN
     if (interpretedParam.block == "KIN") {
         return 18;
     }
@@ -22,6 +20,5 @@ double SMParamSetter::calculateValue(const std::string& name, const Interpreter:
         return 21;
     }
 
-    // Valeur par défaut
-    return 1.0; // Exemple de valeur par défaut
+    return 1.0;
 }
