67bade17a580a67b0716587f6003e9648e7742d5
Theo Reymermier <treymermier@gmx.fr>
2024-11-08 23:01:34 +0100

    add input file to user interface

--- UserInterface/WilsonHandler.cpp
+++ UserInterface/WilsonHandler.cpp
@@ -18,6 +18,7 @@ void print_wilson_usage() {
               << "  --Q/-Q <value>                        : Set Q scale\n"
               << "  --n_flavor/-f <value> (optional)      : Set the number of flavors (optional)\n"
               << "  --Marty/-M <true|false>               : Use Marty groups (default: false)\n"
+              << "  --input_file/-if <slha_name>               : input file for parameters spectrum\n"
               << "  --order/-o <LO|NLO|NNLO>              : Specify the calculation order (default: LO)\n"
               << "  --help/-h                             : Display this help message\n";
 }
@@ -31,11 +32,10 @@ int handleWilsonOptions(int argc, char* argv[]) {
     int n_flavor = 5;
     bool use_marty = false;
     std::string order = "LO";
-
+    std::string input_file = "Test/InputFiles/testinput_thdm.lha";
     std::vector<std::string> Bcoefficient = {"C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10"};
     std::vector<std::string> BPrimecoefficient = {"CP1", "CP2", "CP3", "CP4", "CP5", "CP6", "CP7", "CP8", "CP9", "CP10", "CPQ1", "CPQ2"};
     std::vector<std::string> BScalarcoefficient = {"CQ1", "CQ2"};
-
     for (int i = 1; i < argc; ++i) {
         std::string arg = argv[i];
         if ((arg == "--model" || arg == "-m") && i + 1 < argc) {
@@ -52,6 +52,8 @@ int handleWilsonOptions(int argc, char* argv[]) {
             n_flavor = std::stoi(argv[++i]);
         } else if ((arg == "--Marty" || arg == "-M") && i + 1 < argc) {
             use_marty = (std::string(argv[++i]) == "true");
+        } else if ((arg == "--input_file" || arg == "-if") && i + 1 < argc) {
+            input_file = argv[++i];
         } else if ((arg == "--order" || arg == "-o") && i + 1 < argc) {
             order = argv[++i];
             if (use_marty && order != "LO") {
@@ -69,8 +71,9 @@ int handleWilsonOptions(int argc, char* argv[]) {
     }
 
     try {
+        MemoryManager::GetInstance(input_file, {0})->init();
         CoefficientManager* manager = CoefficientManager::GetInstance(model_name);
-
+        
         if (group_name.empty()) {
             if (std::find(Bcoefficient.begin(), Bcoefficient.end(), coefficient_name) != Bcoefficient.end()) {
                 group_name = "BCoefficientGroup";
@@ -82,11 +85,10 @@ int handleWilsonOptions(int argc, char* argv[]) {
                 throw std::invalid_argument("Invalid coefficient name: not found in any group.");
             }
         }
-
         if (use_marty) {
             group_name += "Marty";
         }
-
+        std::cout << group_name << std::endl;
         if (group_name == "BCoefficientGroup" || group_name == "BCoefficientGroupMarty") {
             manager->registerCoefficientGroup(group_name, use_marty ? std::make_shared<BCoefficientGroupMarty>() : std::make_shared<BCoefficientGroup>());
         } else if (group_name == "BPrimeCoefficientGroup" || group_name == "BPrimeCoefficientGroupMarty") {
@@ -100,20 +102,26 @@ int handleWilsonOptions(int argc, char* argv[]) {
         manager->setQMatch(group_name, Q_match);
         manager->setMatchingCoefficient(group_name, order);
         manager->setGroupScale(group_name, Q);
-
+        manager->setRunCoefficient(group_name, order);
         auto group = manager->getCoefficientGroup(group_name);
         if (coefficient_name.empty()) {
             for (const auto& elem : *group) {
-                std::complex<double> coeff = elem.second->get_CoefficientMatchingValue(order);
-                std::cout << "Coefficient " << elem.first << " at Q_match = " << Q_match << " and Q = " << Q
-                          << ": " << coeff.real() << " + " << coeff.imag() << "i\n";
+                std::complex<double> coeff_M = elem.second->get_CoefficientMatchingValue(order);
+                std::complex<double> coeff_Q = elem.second->get_CoefficientRunValue(order);
+                std::cout << "Coefficient " << elem.first << " at Q_match = " << Q_match
+                          << ": " << coeff_M.real() << " + " << coeff_M.imag() << "i\n";
+                std::cout << "Coefficient " << elem.first << " at Q = " << Q
+                          << ": " << coeff_Q.real() << " + " << coeff_Q.imag() << "i\n";
             }
         } else {
             auto it = group->find(coefficient_name);
             if (it != group->end()) {
-                std::complex<double> coeff = it->second->get_CoefficientMatchingValue(order);
-                std::cout << "Coefficient " << coefficient_name << " at Q_match = " << Q_match << " and Q = " << Q
-                          << ": " << coeff.real() << " + " << coeff.imag() << "i\n";
+                std::complex<double> coeff_M = it->second->get_CoefficientMatchingValue(order);
+                std::complex<double> coeff_Q = it->second->get_CoefficientRunValue(order);
+                std::cout << "Coefficient " << coefficient_name << " at Q_match = " << Q_match
+                          << ": " << coeff_M.real() << " + " << coeff_M.imag() << "i\n";
+                std::cout << "Coefficient " << coefficient_name << " at Q = " << Q
+                          << ": " << coeff_Q.real() << " + " << coeff_Q.imag() << "i\n";
             } else {
                 std::cerr << "Error: Specified coefficient not found in the group." << std::endl;
                 return 1;
