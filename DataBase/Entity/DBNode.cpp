754444439546e9014f1e75ac539d8af76c919a34
TheoR <treymermier@gmx.fr>
2025-02-18 22:07:14 +0000

    update json part

--- DataBase/Entity/DBNode.cpp
+++ DataBase/Entity/DBNode.cpp
@@ -1,4 +1,4 @@
-#include "Json.h"
+#include "DBNode.h"
 
 Node::Node() = default;
 
@@ -138,67 +138,5 @@ void Node::printScalarYAML(const Value& value) const {
         std::cout << std::get<double>(value);
     } else if (std::holds_alternative<bool>(value)) {
         std::cout << (std::get<bool>(value) ? "true" : "false");
-    }
-}
-
-std::shared_ptr<Node> JSONParser::parse(const std::string& input) const {
-    auto root = std::make_shared<Node>();
-    root->set("value", "key1");
-    root->set(42, "key2");
-    auto child = std::make_shared<Node>();
-    child->set(true, "subkey1");
-    root->set(child, "child");
-    return root;
-}
-
-void JSONParser::writeToFile(const std::string& filename, const std::shared_ptr<Node>& root) const {
-    std::ofstream file(filename);
-    if (!file.is_open()) throw std::runtime_error("Unable to open file for writing");
-
-    std::ostringstream oss;
-    root->printJSONToStream(oss);
-    file << oss.str();
-    file.close();
-}
-
-std::shared_ptr<Node> JSONParser::readFromFile(const std::string& filename) const {
-    std::ifstream file(filename);
-    if (!file.is_open()) throw std::runtime_error("Unable to open file for reading");
-
-    std::ostringstream oss;
-    oss << file.rdbuf();
-    return parse(oss.str());
-}
-
-std::shared_ptr<Node> YAMLParser::parse(const std::string& input) const {
-    auto root = std::make_shared<Node>();
-    root->set("yamlKey", "yamlValue");
-    return root;
-}
-
-void YAMLParser::writeToFile(const std::string& filename, const std::shared_ptr<Node>& root) const {
-    std::ofstream file(filename);
-    if (!file.is_open()) throw std::runtime_error("Unable to open file for writing");
-    root->printYAML();
-    file.close();
-}
-
-std::shared_ptr<Node> YAMLParser::readFromFile(const std::string& filename) const {
-    std::ifstream file(filename);
-    if (!file.is_open()) throw std::runtime_error("Unable to open file for reading");
-
-    std::ostringstream oss;
-    oss << file.rdbuf();
-    return parse(oss.str());
-}
-
-std::unique_ptr<Parser> ParserFactory::createParser(Type type) {
-    switch (type) {
-    case Type::JSON:
-        return std::make_unique<JSONParser>();
-    case Type::YAML:
-        return std::make_unique<YAMLParser>();
-    default:
-        throw std::invalid_argument("Unknown parser type");
     }
 }
