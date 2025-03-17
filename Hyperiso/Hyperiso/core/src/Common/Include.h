4460497455bfeda3245f79ad00db4693f2a6d165
ArthKuntz <niels.fardeau@gmail.com>
2025-03-17 10:54:13 +0100

    Refactor core WIP

---
+++ Hyperiso/Hyperiso/core/src/Common/Include.h
@@ -0,0 +1,11 @@
+#ifndef __INCLUDE_H__
+#define __INCLUDE_H__
+
+#include <memory>
+#include <string>
+#include <map>
+#include <vector>
+#include "Logger.h"
+#include "General.h"
+
+#endif // __INCLUDE_H__
