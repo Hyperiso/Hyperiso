9c51f76b624f5e1bcf9734b56852aa6719ef285c
ArthKuntz <niels.fardeau@gmail.com>
2025-05-26 18:54:11 +0200

    Fix Wilson calculation and access

---
+++ Hyperiso/Hyperiso/core/src/Common/Mapper/WilsonBasisMapper.h
@@ -0,0 +1,13 @@
+#ifndef BWILSON_BASIS_MAPPER_H
+#define BWILSON_BASIS_MAPPER_H
+
+#include "EnumMapper.h"
+#include "GeneralEnum.h"
+
+class WilsonBasisMapper : public EnumMapperBase<WilsonBasis, WilsonBasisMapper> {
+public:
+    static const std::map<WilsonBasis, std::string>& mapping();
+    static const std::map<std::string, WilsonBasis>& inverse_mapping();
+};
+
+#endif
