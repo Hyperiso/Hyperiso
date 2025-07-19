fd446e8a5ae52a7430fe162be19d7f61d387221b
Evrard Emonot <evrard@kisuitech.com>
2025-07-19 22:13:23 +0900

    remove virtual

--- Hyperiso/Hyperiso/core/src/Core/app/main_param_loader.cpp
+++ Hyperiso/Hyperiso/core/src/Core/app/main_param_loader.cpp
@@ -5,9 +5,9 @@ int main(){
 
     ParamBlockLoader loader;
     auto ba = std::make_shared<BlockAccessor>();
-    loader.load(ba,"Assets/default/lha/testInput.flha");
+    loader.load(ba,"../../../../../../Assets/default/lha/testInput.flha");
     LOG_INFO("Loaded blocks:", ba->size());
-    loader.save("Assets/default/lha/testOutput.flha", ba);
+    loader.save("../../../../../../Assets/default/lha/testOutput.flha", ba);
     LOG_INFO("Saved blocks to Assets/default/lha/testOutput.flha");
     
     return 0;
