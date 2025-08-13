11eaf2e2d5d3a201a9ea03a61262f3b2877d892f
TheoR <treymermier@gmx.fr>
2025-08-13 09:48:28 +0200

    add some comments to the examples

--- Hyperiso/Hyperiso/examples_cpp/observable_example.cpp
+++ Hyperiso/Hyperiso/examples_cpp/observable_example.cpp
@@ -7,23 +7,24 @@
 
 int main() {
     Logger::getInstance()->setLevel(Logger::LogLevel::INFO);
-    HyperisoMaster hyp;
-    Config config;
-    config.model = Model::SM;
-    hyp.init("lha/si_input.flha", config);
+    HyperisoMaster hyp; // Create the interface for hyperiso (reading/writing in the lha and all the options we want to use)
+    Config config; // Config struct where we can put all the options we want for Hyperiso (general options)
+    config.model = Model::SM; // The model we want to use, SM by default. If not THDM or SUSY, MARTY is needed.
+    hyp.init("lha/si_input.flha", config); // Initialize program manager with LHA file and the config. Search in the Assets directory if relative path.
     LOG_INFO("HyperisoMaster initialized");
 
     QCDOrder order = QCDOrder::NNLO;
 
-    ParameterProvider pp {ParameterType::SM};
-    LOG_INFO("m_b_pole =", pp("QCD", {5, 2}));
+    ObservableInterface oi; // Initialize interface for observables calculation.
 
-    ObservableInterface oi;
-    oi.add_observable(Observables::BR_B__Xs_mu_mu__LOW_Q2, order, false);
+    oi.add_observable(Observables::BR_B__Xs_mu_mu__LOW_Q2, order, true); // add an observable to the interface, at a specific order. The last argument is used to calculate uncertainty with  all the parameters (if true).
     // oi.add_observable(Observables::BR_B__Xs_mu_mu__HIGH_Q2, order, false);
     // oi.add_observable(Observables::BR_B__Xs_tau_tau__HIGH_Q2, order, false);
 
     LOG_INFO("BR(B > X_s mu mu) (q²=[1, 6] GeV²) =", oi.compute_observable(Observables::BR_B__Xs_mu_mu__LOW_Q2), "+-", oi.compute_uncertainty(Observables::BR_B__Xs_mu_mu__LOW_Q2));
+    //compute_observable calculate and retrieve the value of the observable. compute_uncertainty does the same with the uncertainty.
+
+    
     // LOG_INFO("BR(B > X_s mu mu) (q²>14.2 GeV²) =", oi.compute_observable(Observables::BR_B__Xs_mu_mu__HIGH_Q2), "+-", oi.compute_uncertainty(Observables::BR_B__Xs_mu_mu__HIGH_Q2));
     // LOG_INFO("BR(B > X_s tau tau) (q²>14.2 GeV²) =", oi.compute_observable(Observables::BR_B__Xs_tau_tau__HIGH_Q2), "+-", oi.compute_uncertainty(Observables::BR_B__Xs_tau_tau__HIGH_Q2));
 
--- Hyperiso/Hyperiso/examples_cpp/wilson_example.cpp
+++ Hyperiso/Hyperiso/examples_cpp/wilson_example.cpp
@@ -5,25 +5,32 @@
 #include "WilsonInterface.h"
 
 int main() {
-    auto hyp = HyperisoMaster();
-    Config config_hyp;
-    config_hyp.flags[ExternalFlag::USE_MARTY] = false;
-    config_hyp.model = Model::SM;
-    config_hyp.mty_model_name = "ZPrime";
-    config_hyp.mty_model_path = "/home/theo/hyperiso/Assets/input_files/marty_model/ZPrime.h";
+    auto hyp = HyperisoMaster(); // Create the interface for hyperiso (reading/writing in the lha and all the options we want to use)
 
-    hyp.init("lha/camilia.flha", config_hyp); // Initialize program manager with LHA file
+    Config config_hyp; // Config struct where we can put all the options we want for Hyperiso (general options)
 
-    auto wi = WilsonInterface(); // Initialize interface and build the required groups
+    config_hyp.flags[ExternalFlag::USE_MARTY] = false; // Rather we want to use MARTY (mandatory for new models) or hardcoded function (for THDM, SUSY and SM)
 
-    WilsonBuildConfig config({WGroup::B}, 81, 42, QCDOrder::LO);
+    config_hyp.model = Model::SM; // The model we want to use, SM by default. If not THDM or SUSY, MARTY is needed.
 
-    wi.build(config);
+    config_hyp.mty_model_name = "ZPrime"; // Only if Config.model = Model::Custom, name of the bsm model.
+    config_hyp.mty_model_path = "/home/theo/hyperiso/Assets/input_files/marty_model/ZPrime.h"; // Only if Config.model = Model::Custom, path of the bsm model.
 
-    BlockProxy().log_block(ParameterType::WILSON, "BCoefficients_B_SCALE_STANDARD");
+    hyp.init("lha/camilia.flha", config_hyp); // Initialize program manager with LHA file and the config. Search in the Assets directory if relative path.
+
+    auto wi = WilsonInterface(); // Initialize interface for wilson coefficient calculation.
+
+    WilsonBuildConfig config({WGroup::B}, 81, 42, QCDOrder::LO); // Configuration for the wilsons coefficients. First argument is the name of the group, second is the matching scale, third is the running scale and finally the last is the order for the coefficient.
+
+    wi.build(config); // build all the coefficients using the given config.
+
+    BlockProxy().log_block(ParameterType::WILSON, "BCoefficients_B_SCALE_STANDARD"); // Log a block from the HyperisoMaster (lha or created by the wilsons).
 
     LOG_INFO("Parameters created");
-    LOG_INFO("C9(mu_h) at LO =", wi.getR(WGroup::B, WCoef::C9, QCDOrder::LO, ContributionType::TOTAL));
+    LOG_INFO("C9(mu_h) at LO =", wi.getR(WGroup::B, WCoef::C9, QCDOrder::LO, ContributionType::TOTAL)); //getR -> get running coefficient (getM -> get matching coefficient)
+    // ContributionType can be SM, BSM or TOTAL (SM+BSM)
+    // If you want to add all QCDOrder, use getFR (or get FM for matching)
+    
     LOG_INFO("AGAIN");
     LOG_INFO("C9(mu_h) at NLO =", wi.getR(WGroup::B, WCoef::C9, QCDOrder::NLO, ContributionType::TOTAL));
     LOG_INFO("C9(mu_h) at NNLO =", wi.getR(WGroup::B, WCoef::C9, QCDOrder::NNLO, ContributionType::TOTAL));
