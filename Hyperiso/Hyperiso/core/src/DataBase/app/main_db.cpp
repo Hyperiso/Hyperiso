7b16f81a4dad651bd1c021362f01527afb2e949e
Ryudoro <treymermier@gmx.fr>
2025-09-11 10:24:57 +0200

    update db

--- Hyperiso/Hyperiso/core/src/DataBase/app/main_db.cpp
+++ Hyperiso/Hyperiso/core/src/DataBase/app/main_db.cpp
@@ -1,13 +1,12 @@
-#include "MappingDataBase.h"
+#include "DBManager.h"
 #include <iostream>
 
 int main() {
 
-    auto map = MappingDatabase::getInstance("SM", "../Models/SM/sm.json");
-    const auto& smParams = map->getParams();
-    for (const auto& elem : smParams) {
-        std::cout << elem.first << std::endl;
-        std::cout << elem.second.block << " " << elem.second.pdgCode << std::endl;
-    }
+    auto dbm = DBManager();
+
+    auto node = dbm.read_from_file("/home/theo/hyperiso/Assets/lha/camilia.flha");
+
+    node->printJSON();
     return 0;
 }
